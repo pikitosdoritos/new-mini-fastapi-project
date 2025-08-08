b90aee8d95867eecaae8845697d81e27a6c45f27
pikitosdoritos <fastovetsn@gmail.com>
2025-08-09 00:23:58 +0300

    new changes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, HTTPException
-from typing import Optional, List, Dict
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException, Path, Query, Body
+from typing import Optional, List, Dict, Annotated
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -19,6 +19,14 @@ class PostCreate(BaseModel):
     title: str
     body: str
     author_id: int
+
+class UserCreate(BaseModel):
+    name: Annotated[
+        str, Field(..., title = 'User name', min_length = 2, max_length = 20)
+    ]
+    age: Annotated[
+        int, Field(..., title = 'User age', ge = 1, le = 120)
+        ]
 
 users = [
     {'id': 1, 'name': 'Nikita', 'age': 18}, 
@@ -58,16 +66,33 @@ async def add_item(post: PostCreate) -> Post:
 
     return Post(**new_post)
 
+@app.post('/user/add')
+async def user_add(user: Annotated[
+    UserCreate, 
+    Body(..., example = {
+        'name': 'UserName', 
+        'age': 1
+    })
+    ]) -> User:
+    new_user_id = len(users) + 1
+
+    new_user = {'id': new_user_id, 'name': user.name, 'age': user.age}
+    users.append(new_user)
+
+    return User(**new_user)
 
 @app.get('/items/{id}')
-async def items(id: int) -> Post:
+async def items(id: Annotated[int, Path(...,title = 'Тут вказується id поста', ge = 1, lt = 1000)]) -> Post:
     for post in posts:
         if post['id'] == id:
             return Post(**post)
     raise HTTPException(status_code = 404, detail = 'Post not found')
 
 @app.get('/search')
-async def search(post_id: Optional[int] = None) -> Dict[str, Optional[Post]]:
+async def search(post_id: Annotated[
+    Optional[int], 
+    Query(title = 'Id of post to search for', ge = 1, le = 50)
+]) -> Dict[str, Optional[Post]]:
     if post_id:
         for post in posts:
             if post['id'] == id:
