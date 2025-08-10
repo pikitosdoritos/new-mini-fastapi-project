2da6475bedc6f873c225e7145262a147976bef80
pikitosdoritos <fastovetsn@gmail.com>
2025-08-10 23:20:54 +0300

    new changes

--- main.py
+++ main.py
@@ -1,114 +1,44 @@
-from fastapi import FastAPI, HTTPException, Path, Query, Body
+from fastapi import FastAPI, HTTPException, Path, Query, Body, Depends
 from typing import Optional, List, Dict, Annotated
-from pydantic import BaseModel, Field
+from sqlalchemy.orm import Session
+from models import Base, User, Post
+from database import engine, session_local
+from schemas import UserCreate, PostCreate, PostResponse, User as DbUser
 
 app = FastAPI()
 
-class User(BaseModel):
-    id: int
-    name: str
-    age: int
+Base.metadata.create_all(bind = engine)
 
-class Post(BaseModel):
-    id: int
-    title: str
-    body: str
-    author: User
+def get_db():
+    db = session_local()
+    try:
+        yield db
+    finally:
+        db.close()
 
-class PostCreate(BaseModel):
-    title: str
-    body: str
-    author_id: int
+@app.post('/users/', response_model = DbUser)
+async def create_user(user: UserCreate, db: Session = Depends(get_db)) -> User:
+    db_user = User(name = user.name, age = user.age)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
 
-class UserCreate(BaseModel):
-    name: Annotated[
-        str, Field(..., title = 'User name', min_length = 2, max_length = 20)
-    ]
-    age: Annotated[
-        int, Field(..., title = 'User age', ge = 1, le = 120)
-        ]
+    return db_user
 
-users = [
-    {'id': 1, 'name': 'Nikita', 'age': 18}, 
-    {'id': 2, 'name': 'Vasya', 'age': 28},
-    {'id': 3, 'name': 'Berlond', 'age': 15}
-]
+@app.post('/posts/', response_model = PostResponse)
+async def create_post(post: PostCreate, db: Session = Depends(get_db)) -> Post:
+    db_user = db.query(User).filter(User.id == post.author_id).first()
 
-posts = [
-    {'id': 1, 'title': 'News 1', 'body': 'Text 1', 'author': users[1]},
-    {'id': 2, 'title': 'News 2', 'body': 'Text 2', 'author': users[0]},
-    {'id': 3, 'title': 'News 3', 'body': 'Text 3', 'author': users[2]}
-]
-
-# @app.get('/items')
-# async def items() -> List[Post]:
-#     post_obj = []
-#     for post in posts:
-#         post_obj.append(Post(id = post['id'], title = post['title'], body = post['body']))
-#         return post_obj
-    
-@app.get('/items')
-async def items() -> List[Post]:
-   return [Post(**post) for post in posts]
-
-@app.post('/items/add')
-async def add_item(post: PostCreate) -> Post:
-    author = next((user for user in users if user['id'] == post.author_id), None)
-    if not author:
+    if db_user is None:
         raise HTTPException(status_code = 404, detail = 'User not found')
-    
-    
-    new_post_id = len(posts) + 1
 
-    new_post = ({'id': new_post_id, 'title': post.title, 'body': post.body, 'author': author})
-    
-    posts.append(new_post)
+    db_post = Post(title = post.title, body = post.body, author_id = post.author_id)
+    db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
 
-    return Post(**new_post)
+    return db_post
 
-@app.post('/user/add')
-async def user_add(user: Annotated[
-    UserCreate, 
-    Body(..., example = {
-        'name': 'UserName', 
-        'age': 1
-    })
-    ]) -> User:
-    new_user_id = len(users) + 1
-
-    new_user = {'id': new_user_id, 'name': user.name, 'age': user.age}
-    users.append(new_user)
-
-    return User(**new_user)
-
-@app.get('/items/{id}')
-async def items(id: Annotated[int, Path(...,title = 'Тут вказується id поста', ge = 1, lt = 1000)]) -> Post:
-    for post in posts:
-        if post['id'] == id:
-            return Post(**post)
-    raise HTTPException(status_code = 404, detail = 'Post not found')
-
-@app.get('/search')
-async def search(post_id: Annotated[
-    Optional[int], 
-    Query(title = 'Id of post to search for', ge = 1, le = 50)
-]) -> Dict[str, Optional[Post]]:
-    if post_id:
-        for post in posts:
-            if post['id'] == id:
-                return {'data': Post(**post)}
-        raise HTTPException(status_code = 404, detail = 'Post not found')
-    else:
-        return {'data': None}
-    
-
-
-
-
-# @app.get('/')
-# async def homme() -> dict[str, str]:
-#     return {'data': 'message'}
-
-# @app.get('/contacts')
-# async def contacts() -> int:
-#     return 34
+@app.get('/posts/', response_model = List[PostResponse])
+async def posts(db: Session = Depends(get_db)):
+    return db.query(Post).all()
